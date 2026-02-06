e9c8d9ea1dc325d70caaf0c04b4f9bf0a686c291
Xiaoxuan4096 <Xiaoxuan-CodeSubmitter@outlook.com>
2026-02-06 22:43:58 +0800

    完成MyMatrix2D的撰写。

--- ConsoleMaze.cpp
+++ ConsoleMaze.cpp
@@ -7,5 +7,5 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!\n";
+    
 }
---
+++ MyMatrix/MyMatrix2D.cpp
@@ -0,0 +1,47 @@
+// MyMatrix2D.cpp - Two-dimension Matrix using std::vector as underlying.
+// Written by Xiaoxuan4096. 
+
+// This program is distributed under MIT License.
+// See LICENSE.txt for details.
+
+#include "MyMatrix2D.h"
+
+namespace Xiaoxuan4096 {
+	namespace Basic {
+		// Init Functions.
+		void MyMatrix2D::clear() {
+			data.clear();
+			return;
+		}
+
+		// Edit Functions.
+		void MyMatrix2D::addRow(std::string str) {
+			data.push_back(str);
+			return;
+		}
+		void MyMatrix2D::insertRow(std::string str, size_t pos) {
+			if (pos >= data.size())
+				return;
+			data.insert(std::next(data.begin(), pos), str);
+			return;
+		}
+		void MyMatrix2D::deleteRow(size_t pos) {
+			if (pos >= data.size())
+				return;
+			data.erase(std::next(data.begin(), pos));
+			return;
+		}
+
+		void MyMatrix2D::editSingleCharacter(size_t row, size_t col, char c) {
+			if (row >= data.size() || col >= data[row].size())
+				return;
+			data[row][col] = c;
+			return;
+		}
+
+		// Get Data.
+		std::string MyMatrix2D::operator[](size_t row) {
+			return row >= data.size() ? "" : data[row];
+		}
+	}
+}
