61c2d7e4020ae8b59427c1fabc8bd648a6aea3b7
Xiaoxuan4096 <232077189+Xiaoxuan4096@users.noreply.github.com>
2026-06-13 10:35:50 +0800

    添加项目文件。

---
+++ MyMatrix2D/MyMatrix2D.cpp
@@ -0,0 +1,61 @@
+// MyMatrix2D.cpp - Two-dimension char Matrix using std::vector as underlying.
+// Written by Xiaoxuan4096. 
+
+// This program is distributed under MIT License.
+// See LICENSE.txt for details.
+
+#include "MyMatrix2D.h"
+
+namespace Xiaoxuan4096 {
+	// Init Functions.
+	void MyMatrix2D::clear() {
+		data.clear();
+		return;
+	}
+
+	// Edit Functions.
+	void MyMatrix2D::addRow(std::string str) {
+		data.push_back(str);
+		return;
+	}
+	void MyMatrix2D::insertRow(std::string str, size_t pos) {
+		if (pos >= data.size())
+			return;
+		data.insert(std::next(data.begin(), pos), str);
+		return;
+	}
+	void MyMatrix2D::deleteRow(size_t pos) {
+		if (pos >= data.size())
+			return;
+		data.erase(std::next(data.begin(), pos));
+		return;
+	}
+
+	// Get Data.
+	std::string& MyMatrix2D::operator[](size_t row) {
+		return data[row];
+	}
+	size_t MyMatrix2D::getRowCount() {
+		return data.size();
+	}
+	size_t MyMatrix2D::getMinColCount() {
+		if (data.empty())
+			return 0;
+
+		size_t minColCount = data[0].size();
+		for (size_t i = 1; i < data.size(); i++) // If data.size() == 1, this for loop will not be executed.
+			minColCount = minColCount > data[i].size() ? data[i].size() : minColCount;
+
+		return minColCount;
+	}
+	size_t MyMatrix2D::getMaxColCount() {
+		if (data.empty())
+			return 0;
+
+		size_t maxColCount = data[0].size();
+		for (size_t i = 1; i < data.size(); i++) // If data.size() == 1, this for loop will not be executed.
+			maxColCount = maxColCount < data[i].size() ? data[i].size() : maxColCount;
+
+		return maxColCount;
+	}
+}
---
+++ MyMatrix2D/MyMatrix2D.h
@@ -0,0 +1,37 @@
+// MyMatrix2D.h - Two-dimension char Matrix using std::vector as underlying.
+// Written by Xiaoxuan4096. 
+
+// This program is distributed under MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#ifdef MYMATRIX2D_EXPORTS
+#define MYMATRIX2D_API __declspec(dllexport)
+#else
+#define MYMATRIX2D_API __declspec(dllimport)
+#endif
+
+#include <vector>
+#include <string>
+
+namespace Xiaoxuan4096 {
+	class MYMATRIX2D_API MyMatrix2D {
+	private:
+		std::vector<std::string> data;
+	public:
+		// Init Functions.
+		void clear();
+
+		// Edit Functions.
+		void addRow(std::string str);
+		void insertRow(std::string str, size_t pos); // Note that an invalid position will make no change to the matrix.
+		void deleteRow(size_t pos); // Note that an invalid position will make no change to the matrix.
+
+		// Get Data.
+		std::string& operator[](size_t row); // Get a line. Note that giving an invalid row is undefined.
+		size_t getRowCount();
+		size_t getMinColCount(); // If data is empty, this will return a 0.
+		size_t getMaxColCount(); // If data is empty, this will return a 0.
+	};
+}
